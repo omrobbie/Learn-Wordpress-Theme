ba3f59f12d743713ea5996795f4be9b207e74990
omrobbie <putu.roby@yahoo.com>
2017-04-06 06:34:23 +0800

    membuat sidebar

--- functions.php
+++ functions.php
@@ -12,4 +12,17 @@
 		register_nav_menu('primary', __('Navigation Menu', 'temaku'));
 	}
 	add_action('after_setup_theme', 'tema_setup_nav');
+
+	function tema_widget_init() {
+		register_sidebar(array(
+			'name'			=> __('Area Widget Utama', 'temaku'),
+			'id'			=> 'kanan',
+			'description'	=> __('Tampil pada bagian kanan website', 'temaku'),
+			'before_widget'	=> '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'	=> '</aside>',
+			'before_title'	=> '<h3 class="widget-title">',
+			'after_title'	=> '</h3>'
+		));
+	}
+	add_action('widgets_init', 'tema_widget_init');
 ?>
---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<?php
+	if(is_active_sidebar('kanan')):
+		dynamic_sidebar('kanan');
+	endif;
+?>
