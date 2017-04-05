721d19ef33e00c015a58b73c5cb88db206bf218b
omrobbie <putu.roby@yahoo.com>
2017-04-06 06:11:42 +0800

    membuat footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer role="footer">
+	<div class="container">
+		<div class="row">
+			<p class="text-center">Copyright &copy; My Wordpress Blog</p>
+		</div>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+
+</body>
+</html>
