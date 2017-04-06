e97f1395775eda908e243633c80f53bb5666c66d
omrobbie <putu.roby@yahoo.com>
2017-04-06 10:54:58 +0800

    tambah informasi tentang post

--- content.php
+++ content.php
@@ -6,6 +6,11 @@
 			<h3 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 		<?php endif; ?>
 		<div class="entry-meta">
+			<span class="meta_author"><?php _e('oleh ', 'temaku'); the_author_posts_link(); ?></span>
+			<span class="meta_date"><?php _e('pada ', 'temaku'); the_time('d F Y'); ?></span>
+			<span class="meta_comments"><a href="<?php comments_link(); ?>">
+				<?php comments_number('0 Komentar', '1 Komentar', '% Komentar'); ?>
+			</a></span>
 			<?php edit_post_link(__('Edit', 'temaku'), '<span class="edit-link">', '</span>'); ?>
 		</div>
 	</header>
